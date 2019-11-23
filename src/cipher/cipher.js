d7d62825cb950176b08896421b473733740aba7a
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-11-23 11:04:12 +0100

    I finally succeeded to fix cipher bugs

--- src/cipher/cipher.js
+++ src/cipher/cipher.js
@@ -1,17 +1,19 @@
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 const cipher = (word, k) => {
-  let result = word.map((l) => {
+  let result = [...word].map((w) => {
+    let l = w.toLowerCase();
+
     let ix = letters.indexOf(l);
     if (ix == -1) {
       return l;
     }
 
     let nix = (ix + k) % 26;
-    return letters[nix];
+    return (l == w) ? letters[nix] : letters[nix].toUpperCase();
   });
 
-  return result;
+  return result.join('');
 }
 
 export default cipher;
